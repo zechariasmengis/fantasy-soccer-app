b813fdfdfd2bae2b01f3090c76f9b14868c200df
Zecharias Mengis <zmengis@gmail.com>
2021-04-21 11:41:24 -0500

    Added basic navbar

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -17,6 +17,16 @@ class LeaguesController < ApplicationController
         redirect_to league_path(league.id)
     end
 
+    def edit
+        @league = League.find(params[:id])
+    end
+
+    def update
+        @league = League.find(params[:id])
+        @league.update(league_params)
+        redirect_to league_path(@league)
+    end
+
     private
 
     def league_params
