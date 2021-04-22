fa7c3e20fecb77184dda22c5dbb610728404f1db
Zecharias Mengis <zmengis@gmail.com>
2021-04-22 11:35:32 -0500

    added validation error message to new teamplayer form

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -10,8 +10,13 @@ class TeamPlayersController < ApplicationController
     end
 
     def create
-        team_player = TeamPlayer.create(team_player_params)
-        redirect_to team_path(team_player[:team_id])
+        @team_player = TeamPlayer.create(team_player_params)
+        if @team_player.valid?
+            redirect_to team_path(@team_player[:team_id])
+        else
+            flash[:errors] = @team_player.errors.full_messages
+            redirect_back(fallback_location: team_path(@team_player[:team_id]))
+        end   
     end 
 
     private
