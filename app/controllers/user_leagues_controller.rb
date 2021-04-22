9a8fcabef5184b15e01a67d85a4a558058b7826c
Zecharias Mengis <zmengis@gmail.com>
2021-04-22 17:02:57 -0500

    added ability to register team into league

--- app/controllers/user_leagues_controller.rb
+++ app/controllers/user_leagues_controller.rb
@@ -1,2 +1,19 @@
 class UserLeaguesController < ApplicationController
+
+    def new
+        @users = User.all
+        @leagues = League.all
+        @user_league = UserLeague.new
+    end
+
+    def create
+        user_league = UserLeague.create(user_league_params)
+        redirect_to league_path(user_league.league_id)
+    end
+
+    private
+
+    def user_league_params
+        params.require(:user_league).permit(:user_id, :league_id)
+    end
 end
